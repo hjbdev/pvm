060f81f7ddb4f616c89bf20c39085e47ae1a5fb4
Jirat Jaturanpinyo <143077173+MeowSky49887@users.noreply.github.com>
2025-01-28 22:28:44 +0700

    Update help.go

--- src/commands/help.go
+++ src/commands/help.go
@@ -7,7 +7,7 @@ import (
 
 func Help(notFoundError bool) {
 	theme.Title("pvm: PHP Version Manager")
-	theme.Info("Version 1.2.0")
+	theme.Info("Version 1.3.0")
 
 	if notFoundError {
 		theme.Error("Command not found")
